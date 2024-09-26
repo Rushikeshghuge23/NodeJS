a4fa4a5f13173eae3612dfaa38fdc0e4f3cca8e7
Rushikeshghuge23 <rushikeshg2323@gmail.com>
2024-09-27 01:16:47 +0530

    Shift mongodb to online server

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const app = express();
 const db = require('./db');
+require('dotenv').config();
 const Person = require('./models/Person');
 const bodyParser = require('body-parser');
 const MenuItem = require('./models/MenuItem');
 //import routers
 const personRoutes = require('./routes/personRoutes');
 const menuItemRoutes = require('./routes/menuItemRoutes');
+
+const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -18,6 +21,6 @@ app.get('/', function (req, res) {
 app.use('/person', personRoutes);
 app.use('/menu',menuItemRoutes);
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log('listening on port 3000');
 })
